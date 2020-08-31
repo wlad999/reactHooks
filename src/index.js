cb41f92f2aed1214f8cb5ac2a8594db888f95fa2
wlad999 <wlad25888@gmail.com>
2020-08-31 18:28:49 +0300

    97 useState added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+    return <div><HookSwitcher/></div>
+}
+const HookSwitcher = () => {
+    const [color, setColor] = useState('gray')
+    const [fontSize, setFontSize] = useState(14)
+
+    return (
+        <div style={{padding: '10px', backgroundColor: color, fontSize: `${fontSize}px`}}>
+            HELLO WORLD
+            <button onClick={() => {
+                setColor('gray')
+            }}>
+                DARK
+            </button>
+            <button onClick={() => {
+                setColor('white')
+            }}>
+                LIGHT
+            </button>
+            <button onClick={() => {
+                setFontSize((s) => s + 2)
+            }}>+
+            </button>
+        </div>
+    )
+}
+const Person = () => {
+    const [person, setPerson] = useState({
+        firstName: 'Bob',
+        lastName: 'Smith'
+    })
+    setPerson((person) => {
+        return {...person, firstName: 'Mike'}
+    })
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
